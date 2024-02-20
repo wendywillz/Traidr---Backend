d9d74a47e36b0006c33c831d3f5459b85b63178a
fayolistic <olamiposiadekola1@gmail.com>
2024-02-20 01:21:15 +0100

    final setup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,3 @@
-import express from 'express';
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res,) {
-  res.render('index', { title: 'Express' });
-});
-
-export default router;
-
 import express from "express";
 const router = express.Router();
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,13 +1,3 @@
-import express from 'express';
-const router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res,) {
-  res.send('respond with a resource');
-});
-
-export default router;
-
 import express from "express";
 const router = express.Router();
 
