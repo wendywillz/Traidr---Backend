cb35cd102424d98613c1b3a2d15b46b101e00a01
xeth99 <you@examemmanuelfemi01@gmanil.com>
2024-02-25 20:53:21 +0100

    Committing changes before pull

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userController_1 = require("../controller/userController");
+const otp_1 = require("../controller/otp");
 const router = express_1.default.Router();
 /* GET users listing. */
 router.get("/", function (req, res) {
@@ -14,4 +15,8 @@ router.get("/", function (req, res) {
 router.post('/createUser', userController_1.createUser);
 // Endpoint for creating a new user using Google Sign-In
 router.post('/createGoogleUser', userController_1.createGoogleUser);
+// Enpoint for sending OTP
+router.post('/send-otp', otp_1.customerOtp);
+// Enpoint for verifying OTP
+router.post('/verify-otp', otp_1.verifyCustomerOtp);
 exports.default = router;
