94e107997c3491deef06a438b1de42c77e8e82cc
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-06 20:07:22 +0100

    modified the shop model

--- src/controller/shopController.ts
+++ src/controller/shopController.ts
@@ -1,6 +1,6 @@
 import ShopModel from '../model/shop';
 import { Request, Response } from 'express';
-
+import User from '../model/user';
 export const createNewShop = async (req: Request, res: Response): Promise<void> => {
   try {
     console.log("req.body", req.body)
@@ -21,8 +21,16 @@ export const createNewShop = async (req: Request, res: Response): Promise<void> 
       shopZipCode,
       shopOwner
     });
+    const user = await User.findOne({ where: { userId: shopOwner } });
+    if (!user) { 
+      res.json({ userError: 'User not found' });
+    }
+    else {
+      await user.update({ isSeller: true });
+      res.json({ shopCreated: newShop })
+    }
       // console.log("newShop", newShop)
-    res.json({ shopCreated: newShop })
+    
   } catch (error) {
     console.log('Error adding shop:', error)
     res.json({ error: 'Error adding shop' });
