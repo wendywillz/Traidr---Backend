12b5f8d1bf7ba23fe5abac07e4e9523eaaddd15a
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-07 08:38:26 +0100

    adding product end point fixed

--- dist/app.js
+++ dist/app.js
@@ -32,7 +32,7 @@ app.use((0, cors_1.default)({
 }));
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, "../", 'public')));
 app.use('/', index_1.default);
--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 import Product from '../model/product';
-import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export const addNewProduct = async (req: Request, res: Response): Promise<void> => {
- try {
+  try {
+   const {shopId} = req.params 
     // Check if photo is provided
     if (!(req.files as unknown as { [fieldname: string]: Express.Multer.File[]; }).productPhoto) {
       res.json({ noPhoto: 'Photo is required' });
@@ -11,12 +16,15 @@ export const addNewProduct = async (req: Request, res: Response): Promise<void> 
       // Process the photo and video files as needed
       const photo = (req.files as unknown as { [fieldname: string]: Express.Multer.File[]; }).productPhoto[0];
       const video = (req.files as unknown as { [fieldname: string]: Express.Multer.File[]; }).productVideo ? (req.files as unknown as { [fieldname: string]: Express.Multer.File[]; }).productVideo[0] : null;
-      const { productTitle, productDescription, productCategory, productPrice, shopId } = req.body;
+      const { productTitle, productDescription, productCategory, productPrice } = req.body;
       const insertPrice = parseFloat(productPrice);
 
       // Construct the full path to the uploaded files
-      const photoPath = path.join(__dirname, "../../public/uploads", photo.filename);
-      const videoPath = video ? path.join(__dirname, "../../public/uploads", video.filename) : null;
+      //const photoPath = path.join(__dirname, "../../public/uploads", photo.filename);
+      const photoPath = `${BACKEND_URL}/uploads/${photo.filename}`;
+      const videoPath =video ? `${BACKEND_URL}/uploads/${video.filename}` : null;
+      
+      //const videoPath = video ? path.join(__dirname, "../../public/uploads", video.filename) : null;
       console.log("photoPath", photoPath, "videoPath", videoPath)
       // Create a new product
       await Product.create({
@@ -36,3 +44,27 @@ export const addNewProduct = async (req: Request, res: Response): Promise<void> 
     res.json({ error: 'Error uploading files' });
  }
 };
+
+export const getProductsByShopId = async (req: Request, res: Response): Promise<void> => { 
+  try {
+    const { shopId } = req.params;
+    const products = await Product.findAll({ where: { shopId } });
+    console.log("products")
+    res.json({ products });
+  } catch (error) {
+    console.log('Error getting products:', error)
+    res.json({ error: 'Error getting products' });
+  }
+}
+
+export const getAllProducts = async (req: Request, res: Response): Promise<void> => { 
+  try {
+
+    const products = await Product.findAll();
+    console.log("products")
+    res.json({ products });
+  } catch (error) {
+    console.log('Error getting products:', error)
+    res.json({ error: 'Error getting products' });
+  }
+}
