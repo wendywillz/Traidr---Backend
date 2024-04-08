0fcc84a780eaebf09bd4e8fed10d16081cb8f7b1
fayolistic <olamiposiadekola1@gmail.com>
2024-04-08 17:43:25 +0100

    database.config added

---
+++ src/database/database.config.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+const DB = new Sequelize("wqsmzjza", "wqsmzjza", "f-_9BIwS8-atgSnUKRAvJZ_7BPti-uAM",{
+  dialect: 'postgres',
+  host: 'trumpet.db.elephantsql.com',
+  port: 5432,
+  logging: false
+ 
+ 
+})
+export default DB;
+
+
+
