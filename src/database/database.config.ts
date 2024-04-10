ca0c23cf668cb98a0b1c915f125ac627964e8760
Elephant Girl <elephantgirlworks@gmail.com>
2024-04-10 11:37:06 +0100

    minor change made

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,5 +1,29 @@
+import { Sequelize } from 'sequelize'
+
+const DB = new Sequelize('fzuuapxy','fzuuapxy','QRiT7cxPfcXaP-sgvRjSrLZIgnJYxfQY', 
+  {
+    host: "flora.db.elephantsql.com",
+    dialect: "postgres",
+    port: 5432,
+    logging: false,
+   
+    pool: {
+      max:  5,
+      min:  0,
+      idle:  300000,
+      acquire:  10000,
+    },
+  }
+);
+
+export default DB;
+
+/*
+APi key: fff263ab-c52e-4f3f-bd2b-b8040dcac78b
+*/
 
 
+/*
 import { Sequelize } from "sequelize";
 const DB = new Sequelize("wqsmzjza", "wqsmzjza", "f-_9BIwS8-atgSnUKRAvJZ_7BPti-uAM",{
   dialect: 'postgres',
@@ -10,6 +34,7 @@ const DB = new Sequelize("wqsmzjza", "wqsmzjza", "f-_9BIwS8-atgSnUKRAvJZ_7BPti-u
  
 })
 export default DB;
+*/
 
 
 
